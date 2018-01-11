514086ef2b046ac156e0b49928e866c6a6cbf5c0
Animcogn <corbanvilla@gmail.com>
2018-01-10 23:34:54 -0700

    Ajax call works, loops through data adding to map

--- site/public/js/client.js
+++ site/public/js/client.js
@@ -1,30 +1,5 @@
-/*
-var data = [{"field":"Data","type":"date"},  {"field":"Numero","type":"number"}];
-
-var columns = {};
-
-var index = 0;
-
-$.each(data, function() {
-
-    columns[index] = {
-        field : this.field,
-        type : this.type
-    };
-
-    index++;
-});
-
-console.log(columns);
-//console.log(columns.field)
-*/
-
 //Google Maps - DO NOT TOUCH
-var options = {
-   host: 'vpn.unicornsforlife.com',
-   port: '8080',
-   path: '/index.htm'
-};
+var markers = [];
 
 function initMap() {
         var city = {lat: 40.296898, lng: -111.694647};
@@ -33,41 +8,26 @@ function initMap() {
         // Create a map object and specify the DOM element for display.
         var map = new google.maps.Map(document.getElementById('map'), {
           center: city,
-          zoom: 12
+          zoom: 15
         });
 
-        // Create a marker and set its position.
-        var marker = new google.maps.Marker({
-          map: map,
-          position: city,
-          title: 'Hello World!'
-        });
-      }
-
-//Ajax
-$(document).ready(function(){
-    $("button").click(function(){
+        //Ajax
         $.get("database", function(data, status){
-            console.log(data);
-            console.log("calling func storeAjax")
-            storeAjax(JSON.parse(data));
+            (function drop() {
+              for (var i = 0; i < data.length; i++) {
+                addMarker(data[i].latitude, data[i].longitude, i * 400);
+              }
+          }) ();
+
         });
-    });
 
-});
-
-function storeAjax(data){
-    var columns = {};
-    var index = 0;
-    $.each(data, function() {
-        columns[index] = {
-            name : this.name,
-            vendor: this.vendor,
-            first_seen: this.first_seen,
-            last_seen: this.last_seen
-        };
-        index++;
-    });
-    console.log(columns);
-    console.log(columns[0].uuid);
+        function addMarker(latitude, longitude, timeout) {
+            window.setTimeout(function() {
+                markers.push(new google.maps.Marker({
+                    position: {lat: latitude, lng: longitude},
+                    map: map,
+                    animation: google.maps.Animation.DROP
+                }));
+            }, timeout);
+        }
 }
