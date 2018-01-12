339addc42175dbfe5dd585c88cb6c683603a8707
Animcogn <corbanvilla@gmail.com>
2018-01-11 23:56:14 -0700

    parses ajax response to add info to markers

--- site/public/js/client.js
+++ site/public/js/client.js
@@ -15,19 +15,43 @@ function initMap() {
         $.get("database", function(data, status){
             (function drop() {
               for (var i = 0; i < data.length; i++) {
-                addMarker(data[i].latitude, data[i].longitude, i * 400);
+                addMarker(data[i].name, data[i].latitude, data[i].longitude, i * 400, i,
+                    data[i].vendor, data[i].first_seen, data[i].last_seen, data[i].threat_rating);
               }
           }) ();
-
         });
 
-        function addMarker(latitude, longitude, timeout) {
+        function addMarker(title, latitude, longitude, timeout, i, vendor, first_seen, last_seen, rating) {
             window.setTimeout(function() {
                 markers.push(new google.maps.Marker({
                     position: {lat: latitude, lng: longitude},
                     map: map,
-                    animation: google.maps.Animation.DROP
+                    animation: google.maps.Animation.DROP,
+                    title: title
                 }));
+                contentString = customContent(title, vendor, first_seen, last_seen, rating);
+                function customContent(title, vendor, first_seen, last_seen, rating) {
+                    var contentString = '<div id="content">' +
+                        '<h1 id="firstHeading" class="firstHeading">' + 'Device Name: ' + title + '</h1>' +
+                        '<div id="bodyContent">'+
+                        '<p>' + 'Vendor: ' + vendor + '</p>'+
+                        '<p>' + 'First Seen: ' + first_seen + '</p>'+
+                        '<p>' + 'Last Seen: ' + last_seen + '</p>'+
+                        '<p>' + 'Threat Rating: ' + rating + '</p>'+
+                        '<p>' + 'Reason: ' + //added in future
+                        '</div>';
+                        return (contentString);
+                    }
+
+                var infowindow = new google.maps.InfoWindow({
+                    content: contentString
+                });
+
+                console.log(markers[i]);
+                markers[i].addListener('click', function() {
+                    infowindow.open(map, markers[i]);
+                });
             }, timeout);
+
         }
 }
